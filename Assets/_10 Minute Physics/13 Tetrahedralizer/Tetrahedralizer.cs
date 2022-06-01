eecec0e8ee76243acb2d9d251ff331b59b915d94
habrador <erik.nordeus@gmail.com>
2022-06-01 15:31:56 +0200

    Started adding main tetra algorithm

--- Assets/_10 Minute Physics/13 Tetrahedralizer/Tetrahedralizer.cs
+++ Assets/_10 Minute Physics/13 Tetrahedralizer/Tetrahedralizer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 //Based on https://github.com/matthias-research/pages/blob/master/tenMinutePhysics/BlenderTetPlugin.py
 public static class Tetrahedralizer
 {
-    private static int[,] tetFaces = new int[,] { { 2, 1, 0 }, { 0, 1, 3 }, { 1, 2, 3 }, { 2, 0, 3 } };
+    private static readonly int[,] tetFaces = new int[,] { { 2, 1, 0 }, { 0, 1, 3 }, { 1, 2, 3 }, { 2, 0, 3 } };
 
 
     //Called CreateTets in python code
@@ -152,20 +152,176 @@ public static class Tetrahedralizer
 
 
 
-    private static void CreateTetIds(List<Vector3> tetVerts, CustomMesh inputMesh, float minQuality)
-    {   
+    private static void CreateTetIds(List<Vector3> verts, CustomMesh inputMesh, float minQuality)
+    {
+        //pos in verts list (4 per tetra in the list?) (4 * tetNr + 0) to get the first vertex 
+        List<int> tetIds = new List<int>();
+        List<int> tetMarks = new List<int>();
+        List<int> neighbors = new List<int>();
+
+        //The normals to each tetra triangle
+        List<Vector3> planesN = new List<Vector3>();
+        //What is D??? You calculate it by using Dot(p0, n) where n is the normal and p0 a triangle vertex
+        List<float> planesD = new List<float>();
+
+        int tetMark = 0;
+        int firstFreeTet = -1;
+
+        //How many vertices we have excluding the big tetra we added as first tetra
+        int firstBig = verts.Count - 4;
+
         //Start with the first big tetra
-        
+        tetIds.Add(firstBig + 0);
+        tetIds.Add(firstBig + 1);
+        tetIds.Add(firstBig + 2);
+        tetIds.Add(firstBig + 3);
+
+        tetMarks.Add(0);
+
+        for (int i = 0; i < 4; i++)
+        {
+            neighbors.Add(-1);
+
+            //What is going on here???? 
+            //tetFaces are { { 2, 1, 0 }, { 0, 1, 3 }, { 1, 2, 3 }, { 2, 0, 3 } }
+            //These correspons to the tetras 4 triangles and their orientation, similar to a mesh's triangle indices
+            //So we can get all 4 triangle from the list of all vertices if we have the tetId 
+            Vector3 p0 = verts[firstBig + tetFaces[i, 0]];
+            Vector3 p1 = verts[firstBig + tetFaces[i, 1]];
+            Vector3 p2 = verts[firstBig + tetFaces[i, 2]];
+
+            Vector3 n = Vector3.Cross(p1 - p0, p2 - p0).normalized;
+
+            planesN.Add(n);
+            planesD.Add(Vector3.Dot(p0, n));
+        }
+
+        //Vector3 center = Vector3.zero;
+
+
+        //
+        // Main tetrahedralization algorithm starts here
+        //
+
 
         //Add all input points one-by-one
+        for (int i = 0; i < firstBig; i++)
+        {
+            Vector3 p = verts[i];
+
+            if (i % 100 == 0)
+            {
+                Debug.Log($"Inserting vert { i + 1 } of { firstBig }");
+            }
+
+
+            //Find non-deleted tetra
+            int tetNr = 0;
+
+            int safety = 0;
+            
+            while (tetIds[4 * tetNr] < 0)
+            {
+                tetNr += 1;
+
+                safety += 1;
+
+                if (safety > 1000000)
+                {
+                    Debug.Log("Stuck in infinite loop when finding non-deleted tet");
+
+                    break;
+                }
+            }
+
 
             //Search for the tetra the point is in
+            tetMark += 1;
 
+            bool found = false;
+
+            safety = 0;
+
+            while (!found)
+            {
+                safety += 1;
+
+                if (safety > 1000000)
+                {
+                    Debug.Log("Stuck in infinite loop when searchig for the tetra the point is in");
+
+                    break;
+                }
+
+                if (tetNr < 0 || tetMarks[tetNr] == tetMark)
+                {
+                    break;
+                }
+
+                tetMarks[tetNr] = tetMark;
+
+                int id0 = tetIds[4 * tetNr + 0];
+                int id1 = tetIds[4 * tetNr + 1];
+                int id2 = tetIds[4 * tetNr + 2];
+                int id3 = tetIds[4 * tetNr + 3];
+
+                Vector3 center = (verts[id0] + verts[id1] + verts[id2] + verts[id3]) * 0.25f;
+
+                float minT = float.MaxValue;
+
+                int minFaceNr = -1;
+
+                for (int j = 0; j < 4; j++)
+                {
+                    Vector3 n = planesN[4 * tetNr + j];
+                    float d = planesD[4 * tetNr + j];
+
+                    float hp = Vector3.Dot(n, p) - d;
+                    float hc = Vector3.Dot(n, center) - d;
+
+                    float t = hp - hc;
+
+                    if (t == 0f)
+                    {
+                        continue;
+                    }
+
+                    //Time when c -> p hits the face
+                    t = -hc / t;
+
+                    if (t >= 0f && t < minT)
+                    {
+                        minT = t;
+                        minFaceNr = j;
+                    }
+                }
+                
+                if (minT >= 1f)
+                {
+                    found = true;
+                }
+                else
+                {
+                    tetNr = neighbors[4 * tetNr + minFaceNr];
+                }
+            }
+
+            if (!found)
+            {
+                Debug.Log("Failed to insert vertex");
+                
+                continue;
+            }
+
+
+            //Find violatig tets
             //Flood-fill from that tetra to find the tetras that should be removed
 
             //Remove the tetras
 
             //Add a tetra-fan at the new point
+
+        }
 
 
         //Remove the tetras we dont want in the result
