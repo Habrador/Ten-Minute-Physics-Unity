1dea2461573144b0fd3c8cbaf7cc4f578ae89c74
habrador <erik.nordeus@gmail.com>
2022-09-12 09:04:11 +0200

    Refactored code and added actual center of mass calculations

--- Assets/Bonus/3-body-problem/ThreeBodyProblemController.cs
+++ Assets/Bonus/3-body-problem/ThreeBodyProblemController.cs
@@ -28,19 +28,22 @@ public class ThreeBodyProblemController : MonoBehaviour
 
     private const int NUMBER_OF_PLANETS = 5;
 
-    private readonly Vector2 minMaxplanetRadius = new (0.4f, 0.4f);
+    private readonly MinMax minMaxPlanetRadius = new (0.5f, 1.0f);
 
     //Simulation settings
     private readonly int subSteps = 1;
 
+    //Newton's law of universal gravitation
+    
     //Gravitational constant G
     //private readonly float G = 6.674f * Mathf.Pow(10f, -11f); 
     //Our planets masses are small, so we need a much larger G, or no movement will happen
-    private readonly float G = 100f;
+    private readonly float G = 50f;
 
     //The square distance at which the equation is valid
-    private readonly float rSqrMin = 0.2f;
-    private readonly float rSqrMax = 50f;
+    //The planets can intersect so Force will go to infinity if we don't clamp it
+    //Also planets far away from the action will never come back if we don't clamp the max value
+    private readonly MinMax minMaxRSqr = new(0.3f, 25f);
 
 
 
@@ -48,11 +51,10 @@ public class ThreeBodyProblemController : MonoBehaviour
     {
         Random.InitState(SEED);
 
-
         //Generate the planets
         AddPlanets();
 
-        //Give each ball a velocity
+        //Give each planet a velocity
         //foreach (Planet p in allPlanets)
         //{
         //    float maxVel = 0.5f;
@@ -64,9 +66,6 @@ public class ThreeBodyProblemController : MonoBehaviour
 
         //    p.vel = randomVel;
         //}
-
-
-        Vector3 center = GetCenter();
 
         //Center the camera
         thisCamera = Camera.main;
@@ -83,45 +82,55 @@ public class ThreeBodyProblemController : MonoBehaviour
             p.UpdateVisualPosition();
         }
 
-        Vector3 center = GetCenter();
+        //Make sure all objects are visible on screen
+        ZoomCamera();
+    }
+
+
+
+    private void LateUpdate()
+    {
+        //Draw the lines connected to the center of mass from each planet
+        Vector3 center = GetCenterOfMass();
+
+        List<Vector3> lineSegments = new ();
 
         foreach (Planet p in allPlanets)
         {
-            Debug.DrawLine(center, p.pos);
+            lineSegments.Add(p.pos);
+            lineSegments.Add(center);
         }
 
-
-        //Make sure all objects are visible on screen
-        ZoomCamera();
-
-        //Make sure the camera is looking at the center of mass of all planets
-        CenterCamera();
+        DisplayShapes.DrawLineSegments(lineSegments, DisplayShapes.ColorOptions.White);
     }
 
 
 
     //Calculate the center of mass of all planets, which should be constant throughout the simulation
-    //Assume mass are all the same
-    private Vector3 GetCenter()
+    private Vector3 GetCenterOfMass()
     {
-        Vector3 center = Vector3.zero;
+        Vector3 centerOfMass = Vector3.zero;
+
+        float totalMass = 0f;
 
         foreach (Planet p in allPlanets)
         {
-            center += p.pos;
+            centerOfMass += p.pos * p.mass;
+
+            totalMass += p.mass;
         }
 
-        center /= allPlanets.Count;
+        centerOfMass /= totalMass;
 
-        return center;
+        return centerOfMass;
     }
 
 
 
-    //Make sure the camera is focusing on the center of mass of all planets 
+    //Focus the camera on the center of mass of all planets 
     private void CenterCamera()
     {
-        Vector3 center = GetCenter();
+        Vector3 center = GetCenterOfMass();
 
         Vector3 cameraPos = center;
 
@@ -149,20 +158,16 @@ public class ThreeBodyProblemController : MonoBehaviour
         }
 
         //Zoom camera
-        float size = thisCamera.orthographicSize;
-
         float zoomSpeed = 0.5f;
 
-        if (!isVisible)
+        if (isVisible)
         {
-            size += zoomSpeed * Time.deltaTime;
+            zoomSpeed *= -1f;
+        }
 
-            //Debug.Log("Zoom out");
-        }
-        else
-        {
-            size -= zoomSpeed * Time.deltaTime;
-        }
+        float size = thisCamera.orthographicSize;
+
+        size += zoomSpeed * Time.deltaTime;
 
         size = Mathf.Clamp(size, 5f, 100f);
 
@@ -206,7 +211,7 @@ public class ThreeBodyProblemController : MonoBehaviour
 
                 //Planets can intersect so rSqr will go to infinity, making F really big, so we need to clamp
                 //We also need to clamp if they are too far apart or F will be really small and the planet will never return
-                rSqr = Mathf.Clamp(rSqr, rSqrMin, rSqrMax);
+                rSqr = Mathf.Clamp(rSqr, minMaxRSqr.min, minMaxRSqr.max);
 
                 float F = G * ((m1 * m2) / rSqr);
 
@@ -245,7 +250,7 @@ public class ThreeBodyProblemController : MonoBehaviour
             GameObject newBallGO = GameObject.Instantiate(planetPrefabGO);
 
             //Random size
-            float randomSize = Random.Range(minMaxplanetRadius.x, minMaxplanetRadius.y);
+            float randomSize = Random.Range(minMaxPlanetRadius.min, minMaxPlanetRadius.max);
 
             newBallGO.transform.localScale = Vector3.one * randomSize;
 
---
+++ Assets/_10 Minute Physics/MinMax.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct MinMax
+{
+    public float min;
+    public float max;
+
+    public MinMax(float min, float max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+}
