576967c1b161fe616ab707471d503533e6d59225
habrador <erik.nordeus@gmail.com>
2023-10-05 11:40:54 +0200

    Started adding Tutorial 20: height field water sim

---
+++ Assets/_10 Minute Physics/20 Height Field Water Simulation/HeightFieldWaterSimController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightFieldWaterSimController : MonoBehaviour
+{
+    //Water columns in a 2.5d grid
+
+}
