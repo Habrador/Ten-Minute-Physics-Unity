288932276d223a6cdc0c73ec922ef63aa78bc08e
habrador <erik.nordeus@gmail.com>
2023-10-12 11:37:03 +0200

    Added basic scene for FLIP fluid sim

---
+++ Assets/_10 Minute Physics/18 FLIP Water Simulator/FLIPFluidSimController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Simukate a fluid where we also include air by using the FLIP method
+//Based on: "How to write a FLIP Water Simulator" https://matthias-research.github.io/pages/tenMinutePhysics/
+public class FLIPFluidSimController : MonoBehaviour
+{
+    //Public
+    public Material fluidMaterial;
+}
