3a1918daf353d1d513bf6ce1915c6be6968b034b
habrador <erik.nordeus@gmail.com>
2023-10-23 15:40:13 +0200

    Refactored code

--- Assets/_10 Minute Physics/18 FLIP Water Simulator/PushParticlesApart.cs
+++ Assets/_10 Minute Physics/18 FLIP Water Simulator/PushParticlesApart.cs
@@ -9,14 +9,14 @@ namespace FLIPFluidSimulator
     public class PushParticlesApart
     {
         private readonly float particleRadius;
-        //To save computations: this.pInvSpacing = 1.0 / (2.2 * particleRadius);
+        //To save computations: invSpacing = 1.0 / (2.2 * particleRadius);
         //We use it because we might use some other cell structure to handle particle-particle collision more efficient
         //To handle particle-particle collision we check the cell the particle is in and surrounding cells. To make sure that works, we may use another spacing... 
-        private readonly float pInvSpacing;
-        //See above why these may differ from numX and numCells, etc
-        private readonly int pNumX;
-        private readonly int pNumY;
-        private readonly int pNumCells;
+        private readonly float invSpacing;
+        //These may differ from the fluid simulation's 
+        private readonly int numX;
+        private readonly int numY;
+        private readonly int numCells;
         //For particle-particle collision
         //How many particles are in each cell?
         private readonly int[] numCellParticles;
@@ -25,7 +25,7 @@ namespace FLIPFluidSimulator
         private readonly int[] cellParticleIds;
 
         //Convert between 2d and 1d array
-        public int To1D(int i, int j) => i + (pNumX * j);
+        public int To1D(int i, int j) => i + (numX * j);
 
 
 
@@ -35,16 +35,16 @@ namespace FLIPFluidSimulator
         
             //To optimize particle-particle collision
             //Which is why we need another grid than the fluid grid
-            this.pInvSpacing = 1f / (2.2f * particleRadius);
+            this.invSpacing = 1f / (2.2f * particleRadius);
 
-            this.pNumX = Mathf.FloorToInt(simWidth * this.pInvSpacing) + 1;
-            this.pNumY = Mathf.FloorToInt(simHeight * this.pInvSpacing) + 1;
+            this.numX = Mathf.FloorToInt(simWidth * this.invSpacing) + 1;
+            this.numY = Mathf.FloorToInt(simHeight * this.invSpacing) + 1;
 
-            this.pNumCells = this.pNumX * this.pNumY;
+            this.numCells = this.numX * this.numY;
 
-            this.numCellParticles = new int[this.pNumCells];
+            this.numCellParticles = new int[this.numCells];
             //Should be one bigger to be on the safe side
-            this.firstCellParticle = new int[this.pNumCells + 1];
+            this.firstCellParticle = new int[this.numCells + 1];
             this.cellParticleIds = new int[maxParticles];
         }
 
@@ -64,11 +64,11 @@ namespace FLIPFluidSimulator
                 float y = particlePos[2 * i + 1];
 
                 //Which cell is this particle in?  
-                int xi = Mathf.Clamp(Mathf.FloorToInt(x * this.pInvSpacing), 0, this.pNumX - 1);
-                int yi = Mathf.Clamp(Mathf.FloorToInt(y * this.pInvSpacing), 0, this.pNumY - 1);
+                int xi = Mathf.Clamp(Mathf.FloorToInt(x * this.invSpacing), 0, this.numX - 1);
+                int yi = Mathf.Clamp(Mathf.FloorToInt(y * this.invSpacing), 0, this.numY - 1);
 
                 //2d array to 1d
-                int cellNr = xi * this.pNumY + yi;
+                int cellNr = xi * this.numY + yi;
                 //int cellNr = To1D()
 
                 //Add 1 to this cell because a particle is in it
@@ -80,7 +80,7 @@ namespace FLIPFluidSimulator
             int first = 0;
 
             //For each cell
-            for (int i = 0; i < this.pNumCells; i++)
+            for (int i = 0; i < this.numCells; i++)
             {
                 first += this.numCellParticles[i];
 
@@ -88,7 +88,7 @@ namespace FLIPFluidSimulator
             }
 
             //Guard
-            this.firstCellParticle[this.pNumCells] = first;
+            this.firstCellParticle[this.numCells] = first;
 
 
             //Fill particles into cells
@@ -98,11 +98,11 @@ namespace FLIPFluidSimulator
                 float y = particlePos[2 * i + 1];
 
                 //Which cell is this particle in?  
-                int xi = Mathf.Clamp(Mathf.FloorToInt(x * this.pInvSpacing), 0, this.pNumX - 1);
-                int yi = Mathf.Clamp(Mathf.FloorToInt(y * this.pInvSpacing), 0, this.pNumY - 1);
+                int xi = Mathf.Clamp(Mathf.FloorToInt(x * this.invSpacing), 0, this.numX - 1);
+                int yi = Mathf.Clamp(Mathf.FloorToInt(y * this.invSpacing), 0, this.numY - 1);
 
                 //2d array to 1d
-                int cellNr = xi * this.pNumY + yi;
+                int cellNr = xi * this.numY + yi;
 
                 this.firstCellParticle[cellNr]--;
 
@@ -112,86 +112,97 @@ namespace FLIPFluidSimulator
 
             //Push particles apart
 
-            float minDist = 2f * particleRadius;
-            float minDistSquare = minDist * minDist;
-
             //The more iterations the fewer the particles are still colliding with each other
             for (int iter = 0; iter < numIters; iter++)
             {
                 //For each particle
                 for (int i = 0; i < numParticles; i++)
                 {
-                    float px = particlePos[2 * i];
-                    float py = particlePos[2 * i + 1];
+                    MoveParticleApart(i, particlePos, particleColor);
+                }
+            }
+        }
 
-                    //Which cell is this particle in?
-                    int pxi = Mathf.FloorToInt(px * this.pInvSpacing);
-                    int pyi = Mathf.FloorToInt(py * this.pInvSpacing);
 
-                    //Check this cell and surrounding cells for particles that can collide
-                    int x0 = Mathf.Max(pxi - 1, 0);
-                    int y0 = Mathf.Max(pyi - 1, 0);
 
-                    int x1 = Mathf.Min(pxi + 1, this.pNumX - 1);
-                    int y1 = Mathf.Min(pyi + 1, this.pNumY - 1);
+        //Move a single particle apart from the particle it is colliding with
+        //The colliding particles will also move
+        private void MoveParticleApart(int particleIndex, float[] particlePos, float[] particleColor)
+        {
+            float px = particlePos[2 * particleIndex];
+            float py = particlePos[2 * particleIndex + 1];
 
-                    for (int xi = x0; xi <= x1; xi++)
+            //Which cell is this particle in?
+            int pxi = Mathf.FloorToInt(px * this.invSpacing);
+            int pyi = Mathf.FloorToInt(py * this.invSpacing);
+
+            //Check this cell and surrounding cells for particles that can collide
+            int x0 = Mathf.Max(pxi - 1, 0);
+            int y0 = Mathf.Max(pyi - 1, 0);
+
+            int x1 = Mathf.Min(pxi + 1, this.numX - 1);
+            int y1 = Mathf.Min(pyi + 1, this.numY - 1);
+
+            for (int xi = x0; xi <= x1; xi++)
+            {
+                for (int yi = y0; yi <= y1; yi++)
+                {
+                    //2d array to 1d 
+                    int cellNr = xi * this.numY + yi;
+
+                    int firstIndex = this.firstCellParticle[cellNr];
+                    int lastIndex = this.firstCellParticle[cellNr + 1];
+
+                    for (int j = firstIndex; j < lastIndex; j++)
                     {
-                        for (int yi = y0; yi <= y1; yi++)
+                        int particleIndexOther = this.cellParticleIds[j];
+
+                        //Dont check the particle itself
+                        if (particleIndexOther == particleIndex)
                         {
-                            //2d array to 1d 
-                            int cellNr = xi * this.pNumY + yi;
-
-                            int firstIndex = this.firstCellParticle[cellNr];
-                            int lastIndex = this.firstCellParticle[cellNr + 1];
-
-                            for (int j = firstIndex; j < lastIndex; j++)
-                            {
-                                int id = this.cellParticleIds[j];
-
-                                //Dont check the particle itself
-                                if (id == i)
-                                {
-                                    continue;
-                                }
-
-                                //The pos of the other particle we want to check collision against
-                                float qx = particlePos[2 * id];
-                                float qy = particlePos[2 * id + 1];
-
-                                //The distance square to this other particle
-                                float dx = qx - px;
-                                float dy = qy - py;
-
-                                float dSquare = dx * dx + dy * dy;
-
-                                //If outside or exactly at the same position
-                                if (dSquare > minDistSquare || dSquare == 0f)
-                                {
-                                    continue;
-                                }
-
-                                //The actual distance to the other particle
-                                float d = Mathf.Sqrt(dSquare);
-
-                                //Push each particle half the distance needed to make them no longer collide
-                                float s = 0.5f * (minDist - d) / d;
-
-                                dx *= s;
-                                dy *= s;
-
-                                //Update their positions
-                                particlePos[2 * i] -= dx;
-                                particlePos[2 * i + 1] -= dy;
-
-                                particlePos[2 * id] += dx;
-                                particlePos[2 * id + 1] += dy;
-
-
-                                //Update particle colors
-                                UpdateColor(particleColor, i, id);
-                            }
+                            continue;
                         }
+
+                        //The pos of the other particle we want to check collision against
+                        float qx = particlePos[2 * particleIndexOther];
+                        float qy = particlePos[2 * particleIndexOther + 1];
+
+                        //The distance square to this other particle
+                        float dx = qx - px;
+                        float dy = qy - py;
+
+                        float dSquare = dx * dx + dy * dy;
+
+                        //The min distance for the particle not to collide
+                        float minDist = 2f * particleRadius;
+
+                        float minDistSquare = minDist * minDist;
+
+                        //If outside or exactly at the same position
+                        if (dSquare > minDistSquare || dSquare == 0f)
+                        {
+                            continue;
+                        }
+
+                        //The actual distance to the other particle
+                        float d = Mathf.Sqrt(dSquare);
+
+                        //Push each particle half the distance needed to make them no longer collide
+                        float s = 0.5f * (minDist - d) / d;
+
+                        dx *= s;
+                        dy *= s;
+
+                        //Update their positions
+                        particlePos[2 * particleIndex] -= dx;
+                        particlePos[2 * particleIndex + 1] -= dy;
+
+                        particlePos[2 * particleIndexOther] += dx;
+                        particlePos[2 * particleIndexOther + 1] += dy;
+
+
+                        //Update particle colors
+                        UpdateColor(particleColor, particleIndex, particleIndexOther);
                     }
                 }
             }
