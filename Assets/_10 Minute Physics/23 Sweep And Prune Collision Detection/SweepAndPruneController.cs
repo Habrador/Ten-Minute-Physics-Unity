b63b88e7f8310e4d21afd97b811d1c17435ede71
habrador <erik.nordeus@gmail.com>
2025-02-25 13:19:59 +0100

    Added basic scene for 23 Sweep And Prunce collision detection

---
+++ Assets/_10 Minute Physics/23 Sweep And Prune Collision Detection/SweepAndPruneController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SweepAndPruneController : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Hello");
+    }
+
+}
