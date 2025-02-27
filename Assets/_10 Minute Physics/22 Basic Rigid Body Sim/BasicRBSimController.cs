824e2765a7db30d0a3a6a2ac3aa37028663c6929
habrador <erik.nordeus@gmail.com>
2025-02-27 11:17:24 +0100

    Started adding tutorial 22 basic rb sim

---
+++ Assets/_10 Minute Physics/22 Basic Rigid Body Sim/BasicRBSimController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Based on "22 How to write a basic rigid body simulator using position based dynamics"
+//from https://matthias-research.github.io/pages/tenMinutePhysics/index.html
+public class BasicRBSimController : MonoBehaviour
+{
+    
+    
+}
