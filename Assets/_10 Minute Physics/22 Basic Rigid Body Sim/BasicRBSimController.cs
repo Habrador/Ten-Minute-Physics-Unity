a958e1c3cade449c5c09ee66f8034f7ba2dac79a
habrador <erik.nordeus@gmail.com>
2025-10-30 19:24:02 +0100

    Moved the code used to drag the physics sim rbs around to its own script because is needed in multiple scenes

--- Assets/_10 Minute Physics/22 Basic Rigid Body Sim/BasicRBSimController.cs
+++ Assets/_10 Minute Physics/22 Basic Rigid Body Sim/BasicRBSimController.cs
@@ -35,9 +35,7 @@ public class BasicRBSimController : MonoBehaviour
     private readonly int numSubSteps = 10;
 
     //Mouse interaction
-    private bool hasSelectedRb = false;
-    private float d;
-    private Camera thisCamera;
+    Interaction interaction;
     
 
 
@@ -46,7 +44,7 @@ public class BasicRBSimController : MonoBehaviour
         //Default scene
         InitScene(Scenes.Chain);
 
-        thisCamera = Camera.main;
+        this.interaction = new Interaction(Camera.main, this.rbSimulator);
     }
 
 
@@ -56,7 +54,7 @@ public class BasicRBSimController : MonoBehaviour
         rbSimulator.MyUpdate();
 
         //Should maybe be in LateUpdate()???
-        MouseInteraction();
+        this.interaction.DragWithMouse();
     }
 
 
@@ -116,84 +114,6 @@ public class BasicRBSimController : MonoBehaviour
         else
         {
             Debug.Log("There's no scene to init!");
-        }
-    }
-
-
-
-    //Interact with the scene by using mouse
-    private void MouseInteraction()
-    {
-        //Try to select rb
-        if (Input.GetMouseButtonDown(0) && hasSelectedRb == false)
-        {
-            //Raycasting
-            Ray ray = thisCamera.ScreenPointToRay(Input.mousePosition);
-            
-            //If we hit a collider
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                //Each gameobject in Unity has a unique identifer which we can use to find it among the rb we simulate
-                int id = hit.transform.gameObject.GetInstanceID();
-
-                //Debug.Log(hit.transform.gameObject.GetInstanceID());
-                //Debug.Log("hit");
-
-                //Find the rigidbody with this id in the list of all rbs in the simulator
-                List<MyRigidBody> allRigidBodies = rbSimulator.allRigidBodies;
-
-                foreach (MyRigidBody thisRb in allRigidBodies)
-                {
-                    //If the ids match
-                    if (thisRb.rbVisualObj.GetInstanceID() == id)
-                    {
-                        //Debug.Log("Identified the rb");
-
-                        //Data
-
-                        //p_m - position where ray intersects with the collider
-                        Vector3 p = hit.point;
-
-                        //d - distance from position we hit to mouse
-                        this.d = hit.distance;
-
-                        //Create a distance constraint
-                        rbSimulator.StartDrag(thisRb, hit.point);
-
-                        hasSelectedRb = true;
-
-                        break;
-                    }
-                }
-            }
-        }
-        
-
-
-        //Drag selected rb
-        if (hasSelectedRb == true)
-        {
-            //On mouse move -> update p by using distance d and new mouse ray
-            Ray ray = thisCamera.ScreenPointToRay(Input.mousePosition);
-
-            //Update p_m using d
-            Vector3 p_m = ray.origin + ray.direction * d;
-        
-            rbSimulator.Drag(p_m);
-
-            //Vector3 p0 = rbSimulator.dragConstraint.worldPos0;
-
-            //Debug.DrawLine(p0, p_m, UnityEngine.Color.blue);
-        }
-        
-
-
-        //Deselect rb
-        if (Input.GetMouseButtonUp(0) && hasSelectedRb == true)
-        {
-            rbSimulator.EndDrag();
-            
-            hasSelectedRb = false;
         }
     }
 
