4c30382b15d82571f003f5088d9e475264bf8b53
habrador <erik.nordeus@gmail.com>
2025-11-12 10:38:48 +0100

    Clarified how angular constraint works

---
+++ Assets/_10 Minute Physics/_XPBD/Constraints/AngularConstraint.cs
@@ -0,0 +1,162 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace XPBD
+{
+    public static class AngularConstraint
+    {
+        //From YT video
+        //delta_phi: orientation correction vector
+        //alpha: compliance
+        //ApplyAngularCorrection(delta_phi, alpha)
+        //{
+        //  C = |delta_phi|
+        //  n = delta_phi / |delta_phi|
+        // 
+        //  w = n^T * I^-1 * n
+        //
+        //  lambda = -C * (w_1 + w_2 + alpha / dt^2)^-1
+        //
+        //  q = q +- 0.5 * lambda * [I^-1 * n, 0] * q
+        //
+        //  Torque = (lambda * n) / dt^2
+        //}
+
+        //In the paper you see
+        //
+        // We have a rotation vector delta_q which we split into:
+        // n: direction n which is the rotation axis
+        // theta: magnitude which is the which is the rotation angle
+        //
+        // w = n^T * I^-1 * n
+        //
+        // delta_lambda = (-theta - (alpha_tilde * lambda)) / (w_1 + w_2 + alpha_tilde)
+        // where alpha_tilde = alpha / dt^2
+        // lambda = lambda + delta_lambda
+        //
+        // q = q +- 0.5 * [I^-1 * p, 0] * q
+        //
+        // Project the quantities n, r, p into the rest state of the bodies before evaluating the expression
+        // For joints, the attachment points r are typically defined in rest state
+        //
+        // Torque: tau = (lambda * n) / dt^2
+
+        //See PositionalConstraint class why we dont have to use delta_lambda!
+
+
+        //For rotational constraints
+        //alpha: inverse of physical stiffness (sometimes called compliance)
+        //delta_phi: rotation vector
+        //rb1, rb2: connected rigid bodies
+        public static float ApplyCorrection(float alpha, Vector3 delta_phi, MyRigidBody rb1, MyRigidBody rb2, bool velocityLevel = false)
+        {
+            //If no correction
+            if (delta_phi.sqrMagnitude == 0f)
+            {
+                return 0f;
+            }
+
+            //Rotation angle
+            float theta = delta_phi.magnitude;
+
+            //Rotation axis
+            Vector3 normal = delta_phi.normalized;
+
+            //This is different for rational constraint,
+            //it uses no pos when calculating generalized inverse mass
+            //Compute generalized inverse mass for each rb
+            // w = n^T * I^-1 * n
+            float w_tot = GeneralizedInverseMass.Calculate(rb1, normal);
+
+            if (rb2 != null)
+            {
+                w_tot += GeneralizedInverseMass.Calculate(rb2, normal);
+            }
+
+            if (w_tot == 0f)
+            {
+                return 0f;
+            }
+
+            //Compute Lagrange multiplier
+            // lambda = -theta * (w_1 + w_2 + alpha / dt^2)^-1
+            float lambda = -theta / (w_tot + (alpha / (rb1.dt * rb1.dt)));
+
+
+            //Update rot
+            //q = q +- 0.5 * lambda * [I^-1 * n, 0] * q
+            Vector3 lambda_normal = normal * -lambda;
+
+            UpdateRot(rb1, lambda_normal, velocityLevel);
+
+            if (rb2 != null)
+            {
+                lambda_normal *= -1f;
+                UpdateRot(rb2, lambda_normal, velocityLevel);
+            }
+
+            //Constraint torque [Nm]
+            //tau = (lambda * n) / dt^2
+            //We dont need direction so ignore n
+            float constraintTorque = lambda / (rb1.dt * rb1.dt);
+
+            return constraintTorque;
+        }
+
+
+        //Update rot
+        //q = q +- 0.5 * lambda * [I^-1 * n, 0] * q = q +- 0.5 * [I^-1 * p, 0] * q
+        //p is lambda_normal
+        public static void UpdateRot(MyRigidBody body, Vector3 p, bool velocityLevel)
+        {
+            if (body.invMass == 0f)
+            {
+                return;
+            }
+
+            //Transform p to local space so we can use the simplifed moment of inertia
+            p = body.invRot * p;
+
+            //Scales dOmega by the inverse inertia
+            //Why is it called dOmega?
+            //I^-1 * p
+            Vector3 dOmega = Vector3.zero;
+
+            dOmega.x = p.x * body.invInertia.x;
+            dOmega.y = p.y * body.invInertia.y;
+            dOmega.z = p.z * body.invInertia.z;
+
+            //Transforms dOmega back into the original coordinate frame after the inverse rotation was applied earlier
+            dOmega = body.rot * dOmega;
+
+            //In the code onn github:
+            // stabilize rotation
+            //dOmega.multiplyScalar(0.5);
+            //and then he multiplies it again by 0.5 (body.rot.x += 0.5f * dRot.x;)
+            //Why 2 times???
+
+            //Same as during Integrate() except for the dt which is now 1
+            //dRot <- dOmega * q_i
+            //[I^-1 * p, 0]
+            Quaternion dRot = new(dOmega.x, dOmega.y, dOmega.z, 0f);
+
+            //[I^-1 * p, 0] * q
+            dRot *= body.rot;
+
+            //q_i = q_i + 0.5 * dRot
+            body.rot.x += 0.5f * dRot.x;
+            body.rot.y += 0.5f * dRot.y;
+            body.rot.z += 0.5f * dRot.z;
+            body.rot.w += 0.5f * dRot.w;
+
+            //Always normalize when we update rotation
+            body.rot.Normalize();
+
+            //Cache the inverse
+            body.invRot = Quaternion.Inverse(body.rot);
+        }
+    }
+}
--- Assets/_10 Minute Physics/_XPBD/Constraints/PositionalConstraint.cs
+++ Assets/_10 Minute Physics/_XPBD/Constraints/PositionalConstraint.cs
@@ -2,32 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 namespace XPBD
 {
+    //Aka Linear constraint
     public static class PositionalConstraint
     {
-        //This doesnt have to be a distance constrain as there are many other constraints...
-        //compliance: inverse of physical stiffness (alpha in equations)
-        //corr: n * C = n * (l - l_0) (direction times elongation)
-        //pos: where the constraint attaches to this body in world space
-        //otherBody: the connected rb (if any, might be a fixed object for collision)
-        //otherPos: where the constraint attaches to the other rb in world space
-        //velocityLevel: Was dt but is replaced by a bool in joints, and determines if we should use dt in some calculations (dt is cached each FixedUpdate)
-        //Returns the force on this constraint
-        public static float ApplyCorrection(float compliance, Vector3 corr, MyRigidBody body, Vector3 pos, MyRigidBody otherBody, Vector3 otherPos, bool velocityLevel = false)
-        {
-            //In the paper you see
-            //delta_lambda = (-c - (alpha_tilde * lambda)) / (w_1 + w_2 + alpha_tilde)
-            //alpha_tilde = alpha / dt^2
-            //lambda = lambda + delta_lambda
-            //and instead of lambda * normal they use delta_lambda * normal
-            //BUT according to the YT video "09 Getting ready to simulate the world with XPBD" (7:15)
-            //we dont need to keep track of the lagrange multiplier per constraint
-            //if we iterate over the constraints just once
-            //Notice that iteration and substeps are not the same
-            //Some are iterating over the constraints multiple times each substep
-            //But we are doing it just once because it generates a better result
+        //From YT video
+        //p1,p2: locations of attachment points
+        //delta_p: correction vector
+        //alpha: compliance
+        //ApplyLinearCorrection(p1, p2, delta_p, alpha)
+        //{
+        //  C = |delta_p|
+        //  n = delta_p / |delta_p|
+        // 
+        //  w = m^-1 * ((p - x) x n)^T * I^-1 * ((p - x) x n)
+        //
+        //  lambda = -C * (w_1 + w_2 + alpha / dt^2)^-1
+        //
+        //  x = x +- m^-1 * lambda * n
+        //  q = q +- 0.5 * lambda * [I^-1 * ((p - x) x n), 0] * q
+        //
+        //  F = (lambda * n) / dt^2
+        //}
 
+
+        //In the paper you see
+        //
+        // To apply a positional constraint delta_x at position r1 and r2, we first split
+        // it into its direction n and its magnitude c
+        //
+        // w = m^-1 * (r x n)^T * I^-1 * (r x n) 
+        // where r = p - x
+        //
+        // delta_lambda = (-c - (alpha_tilde * lambda)) / (w_1 + w_2 + alpha_tilde)
+        // where alpha_tilde = alpha / dt^2
+        // lambda = lambda + delta_lambda
+        //
+        // x = x +- p / m
+        // where p = delta_lambda * n (which is the positional impulse)
+        // q = q +- 0.5 * [I^-1 * (r x p), 0] * q
+        //
+        // Force f = (lambda * n) / dt^2
+
+        //BUT according to the YT video "09 Getting ready to simulate the world with XPBD" (7:15)
+        //we dont need to keep track of the lagrange multiplier per constraint
+        //if we iterate over the constraints just once
+        //Notice that iteration and substeps are not the same
+        //Some are iterating over the constraints multiple times each substep
+        //But we are doing it just once because it generates a better result
+
+
+        //This doesnt have to be a distance constrain as there are many other constraints...
+        //alpha: inverse of physical stiffness (sometimes called compliance)
+        //corr: n * C = n * (l - l_0) (direction times elongation)
+        //p1,p2: where the constraint attaches to this body in world space
+        //rb1, rb2: connected rigid bodies
+        //velocityLevel: Was dt but is replaced by a bool in joints, and determines if we should use dt in some calculations (dt is cached each FixedUpdate) Is related to damping calculations???
+        //Returns the force on this constraint
+        public static float ApplyCorrection(float alpha, Vector3 corr, MyRigidBody rb1, Vector3 p1, MyRigidBody rb2, Vector3 p2, bool velocityLevel = false)
+        {
             //If no elongation
             if (corr.sqrMagnitude == 0f)
             {
@@ -37,15 +73,16 @@ namespace XPBD
             //Find C and n from corr which is C * n
             float C = corr.magnitude;
 
+            //This can be optimized as we alread have the magnitude: normal = corr / C
             Vector3 normal = corr.normalized;
 
             //Compute generalized inverse mass for each rb
             // w = m^-1 * (r x n)^T * I^-1 * (r x n)
-            float w_tot = GeneralizedInverseMass.Calculate(body, normal, pos);
+            float w_tot = GeneralizedInverseMass.Calculate(rb1, normal, p1);
 
-            if (otherBody != null)
+            if (rb2 != null)
             {
-                w_tot += GeneralizedInverseMass.Calculate(otherBody, normal, otherPos);
+                w_tot += GeneralizedInverseMass.Calculate(rb2, normal, p2);
             }
 
             if (w_tot == 0f)
@@ -55,25 +92,25 @@ namespace XPBD
 
             //Compute Lagrange multiplier
             // lambda = -C * (w_1 + w_2 + alpha / dt^2)^-1
-            float lambda = -C / (w_tot + (compliance / (body.dt * body.dt)));
+            float lambda = -C / (w_tot + (alpha / (rb1.dt * rb1.dt)));
 
             //Update pos and rot
-            //x_i = x_i +- w_i * lambda * n
-            //q_i = q_i + 0.5 * lambda * [I_i^-1 * (r_i x n), 0] * q_i
+            //x = x +- 1/m * lambda * n
+            //q = q +- 0.5 * lambda * [I^-1 * (r x n), 0] * q
             Vector3 lambda_normal = normal * -lambda;
 
-            UpdatePosAndRot(body, lambda_normal, pos);
+            UpdatePosAndRot(rb1, lambda_normal, p1);
 
-            if (otherBody != null)
+            if (rb2 != null)
             {
                 lambda_normal *= -1f;
-                UpdatePosAndRot(otherBody, lambda_normal, otherPos);
+                UpdatePosAndRot(rb2, lambda_normal, p2);
             }
 
-            //Constraint force
+            //Constraint force [N]
             //F = (lambda * n) / dt^2
             //We dont need direction so ignore n
-            float constraintForce = lambda / (body.dt * body.dt);
+            float constraintForce = lambda / (rb1.dt * rb1.dt);
 
             return constraintForce;
         }
@@ -86,9 +123,9 @@ namespace XPBD
         // q = q +- 0.5 * (I^-1 * (r x p), 0) * q
         //where the positional impulse p = lambda * n 
         //because we are using lambda and not delta_lambda!
-        public static void UpdatePosAndRot(MyRigidBody body, Vector3 p, Vector3 pos)
+        public static void UpdatePosAndRot(MyRigidBody rb, Vector3 p, Vector3 pos)
         {
-            if (body.invMass == 0f)
+            if (rb.invMass == 0f)
             {
                 return;
             }
@@ -98,32 +135,32 @@ namespace XPBD
             // x = x +- p / m
             // +- Because we move in different directions because we have two rb
             // p already has the +- in it
-            body.pos += body.invMass * p;
+            rb.pos += rb.invMass * p;
 
 
             //Angular correction
             // q = q +- 0.5 * (I^-1 * (r x p), 0) * q
 
             //r
-            Vector3 r = pos - body.pos;
+            Vector3 r = pos - rb.pos;
 
             //r x p
             Vector3 r_cross_p = Vector3.Cross(r, p);
 
             //Transform dOmega to local space so we can use the simplifed moment of inertia
-            r_cross_p = body.invRot * r_cross_p;
+            r_cross_p = rb.invRot * r_cross_p;
 
             //Scales dOmega by the inverse inertia
             //Why is it called dOmega?
             //I^-1 * (r x p)
             Vector3 dOmega = Vector3.zero;
 
-            dOmega.x = r_cross_p.x * body.invInertia.x;
-            dOmega.y = r_cross_p.y * body.invInertia.y;
-            dOmega.z = r_cross_p.z * body.invInertia.z;
+            dOmega.x = r_cross_p.x * rb.invInertia.x;
+            dOmega.y = r_cross_p.y * rb.invInertia.y;
+            dOmega.z = r_cross_p.z * rb.invInertia.z;
 
             //Transforms dOmega back into the original coordinate frame after the inverse rotation was applied earlier
-            dOmega = body.rot * dOmega;
+            dOmega = rb.rot * dOmega;
 
             //Same as during Integrate() except for the dt which is now 1
             //dRot <- dOmega * q_i
@@ -131,19 +168,19 @@ namespace XPBD
             Quaternion dRot = new(dOmega.x, dOmega.y, dOmega.z, 0f);
 
             //[I^-1 * (r x p), 0] * q
-            dRot *= body.rot;
+            dRot *= rb.rot;
 
             //q_i = q_i + 0.5 * dRot
-            body.rot.x += 0.5f * dRot.x;
-            body.rot.y += 0.5f * dRot.y;
-            body.rot.z += 0.5f * dRot.z;
-            body.rot.w += 0.5f * dRot.w;
+            rb.rot.x += 0.5f * dRot.x;
+            rb.rot.y += 0.5f * dRot.y;
+            rb.rot.z += 0.5f * dRot.z;
+            rb.rot.w += 0.5f * dRot.w;
 
             //Always normalize when we update rotation
-            body.rot.Normalize();
+            rb.rot.Normalize();
 
             //Cache the inverse
-            body.invRot = Quaternion.Inverse(body.rot);
+            rb.invRot = Quaternion.Inverse(rb.rot);
         }
     }
 }
