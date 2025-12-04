1fe66a938d397cb291da79447fdcb8f5122826fc
habrador <erik.nordeus@gmail.com>
2025-12-04 10:43:17 +0100

    Started adding fractals for 19!

---
+++ Assets/_10 Minute Physics/19 Diff and Calculus/Fractals/FractalsController.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using Color = UnityEngine.Color;
+
+public class FractalsController : MonoBehaviour
+{
+    public GameObject planeObj;
+
+    private int[][] gradientColors = 
+    {
+        new int[] {15, 2, 66},
+        new int[] {191, 41, 12},
+        new int[] {222, 99, 11},
+        new int[] {229, 208, 14},
+        new int[] {255, 255, 255},
+        new int[] {102, 173, 183},
+        new int[] {14, 29, 104}
+    };
+
+    int maxIters = 100;
+    bool drawMandelbrot = false;
+    float centerX = 0.0f;
+    float centerY = 0.0f;
+    float scale = 0.0035f;
+
+    float juliaX = -0.62580000000000f;
+    float juliaY = 0.40250000000000f;
+
+    //Should we use the sci-color or just plain color
+    bool drawMono = true;
+
+
+    void Start()
+    {
+        //Our canvas is twice as long as it is high
+        int height = 100;
+        int width = 2 * height;
+
+        //Generate the fractal colors
+        Color[,] colors = GenerateColors(width, height);
+
+        //Display the colors on the plane
+    }
+
+
+
+    private Color[,] GenerateColors(int width, int height)
+    {
+        Color[,] colors = new Color[width, height];
+
+        //The start y coordinate in world space
+        float y = centerY - height / 2f * scale;
+
+        for (int j = 0; j < height; j++)
+        {
+            //The start x coordinate in world space
+            float x = centerX - width / 2f * scale;
+
+            for (int i = 0; i < height; i++)
+            {
+                //Compute the color for this pixel
+                int numIters = GetNumIters(x, y, drawMandelbrot ? x : juliaX, drawMandelbrot ? y : juliaY, maxIters);
+
+                if (numIters < maxIters)
+                {
+                    if (drawMono)
+                    {
+                        colors[i, j] = new Color(0, 0, 0);
+                    }
+                    else
+                    {
+                        colors[i, j] = GetGradientColor(numIters, 20);
+                    }
+                }
+                else
+                {
+                    if (drawMono)
+                    {
+                        colors[i, j] = new Color(255, 192, 0);
+                    }
+                    else
+                    {
+                        colors[i, j] = new Color(0, 0, 0);
+                    }
+                }
+            }
+        }
+
+        return colors;
+    }
+
+
+
+    private int GetNumIters(float x1, float x2, float c1, float c2, int maxIters)
+    {
+        for (int iters = 0; iters < maxIters; iters++)
+        {
+            if (x1 * x1 + x2 * x2 > 4.0f)
+            {
+                return iters;
+            }
+
+            float x = x1;
+            x1 = x1 * x1 - x2 * x2;
+            x2 = 2.0f * x * x2;
+
+            x1 += c1;
+            x2 += c2;
+        }
+        return maxIters;
+    }
+
+
+
+    //Get a color 0->255
+    private Color GetGradientColor(int nr, int steps)
+    {
+        int numCols = gradientColors.Length;
+
+        int col0 = Mathf.FloorToInt(nr / steps) % numCols;
+        int col1 = (col0 + 1) % numCols;
+        
+        var step = nr % steps;
+
+        int[] color = { 0, 0, 0 };
+
+        for (var i = 0; i < 3; i++)
+        {
+            var c0 = gradientColors[col0][i];
+            var c1 = gradientColors[col1][i];
+            color[i] = Mathf.FloorToInt(c0 + (c1 - c0) / steps * step);
+        }
+
+        return new Color(color[0], color[1], color[2]);
+    }
+}
